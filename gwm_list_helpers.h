2dea7518984edabf8b46e60a13b8977f7344e0f9
Thomas (nezza-_-) Roth <code@stacksmashing.net>
2011-09-29 16:17:19 +0200

    New list helper GWM_NEXT_IF_FIRST and additional NULL check in GWM_LIST_REMOVE.

--- gwm_list_helpers.h
+++ gwm_list_helpers.h
@@ -1,17 +1,24 @@
 #ifndef GWM_LIST_HELPERS
 #define GWM_LIST_HELPERS 1
 
+#define GWM_NEXT_IF_FIRST(list, list_obj) \
+	if(list == list_obj) { \
+		list = list_obj->next; \
+	}
+
 #define GWM_LIST_REMOVE(list_obj) \
-		if(list_obj->prev) { \
-			list_obj->prev->next = list_obj->next; \
-			if(list_obj->next) { \
-				list_obj->next->prev = list_obj->prev; \
-			} \
-		} \
-		if(list_obj->next) { \
-			list_obj->next->prev = list_obj->prev; \
+		if(list_obj) {\
 			if(list_obj->prev) { \
 				list_obj->prev->next = list_obj->next; \
+				if(list_obj->next) { \
+					list_obj->next->prev = list_obj->prev; \
+				} \
+			} \
+			if(list_obj->next) { \
+				list_obj->next->prev = list_obj->prev; \
+				if(list_obj->prev) { \
+					list_obj->prev->next = list_obj->next; \
+				} \
 			} \
 		}
 
